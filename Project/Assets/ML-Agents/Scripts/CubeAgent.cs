3f552a31a45af8022049554ca7229f8b43f986da
Ayman Faisal <35163891+ayman-faisal@users.noreply.github.com>
2021-12-12 09:11:58 -0800

    Implemented code for puzzle solving

--- Project/Assets/ML-Agents/Scripts/CubeAgent.cs
+++ Project/Assets/ML-Agents/Scripts/CubeAgent.cs
@@ -8,56 +8,161 @@ using Unity.MLAgents.Sensors;
 public class CubeAgent : Agent
 {
     [SerializeField] private Transform targetTransform;
-    [SerializeField] private Transform buttonTransform;
     [SerializeField] private Material rewardMaterial;
     [SerializeField] private Material penaltyMaterial;
     [SerializeField] private MeshRenderer agentMeshRenderer;
+    [SerializeField] private GameObject button;
+    [SerializeField] private GameObject barrier;
+
+    private bool isButtonAvailable;
+    private bool isBarrierGone;
+    private Rigidbody agentRigidBody;
+
+    private void Awake()
+    {
+        agentRigidBody = GetComponent<Rigidbody>();
+    }
 
     public override void OnEpisodeBegin()
     {
+        agentRigidBody.velocity = Vector3.zero;
+
         Vector3 agentPosition = new Vector3(Random.Range(0f, +2f), 0, Random.Range(-0.7f, +0.7f));
         Vector3 buttonPosition = new Vector3(Random.Range(-2f, -0.5f), -0.262f, Random.Range(-0.8f, +0.9f));
         Vector3 targetPosition = new Vector3(Random.Range(-2f, +2f), -0.189f, Random.Range(2.75f, 4.5f));
         
         transform.localPosition = agentPosition;
-        buttonTransform.localPosition = buttonPosition;
+        button.transform.localPosition = buttonPosition;
         targetTransform.localPosition = targetPosition;
+
+        button.SetActive(true);
+        isButtonAvailable = true;
+        barrier.SetActive(true);
+        isBarrierGone = false;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(buttonTransform.position);
+        sensor.AddObservation(isButtonAvailable ? 1 : 0);
+
+        Vector3 dirToButton = (button.transform.localPosition - transform.localPosition).normalized;
+        sensor.AddObservation(dirToButton.x);
+        sensor.AddObservation(dirToButton.z);
+
+        sensor.AddObservation(isBarrierGone ? 1 : 0);
+        if (isBarrierGone)
+        {
+            // Can reach target so set directions to target
+            Vector3 dirToTarget = (targetTransform.localPosition - transform.localPosition).normalized;
+            sensor.AddObservation(dirToTarget.x);
+            sensor.AddObservation(dirToTarget.z);
+        }
+        else
+        {
+            // Can not reach target so x & z are 0
+            sensor.AddObservation(0f);
+            sensor.AddObservation(0f);
+        }
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
+        Vector3 addForce = new Vector3(0, 0, 0);
+        
+        int moveX = actions.DiscreteActions[0];
+        switch (moveX)
+        {
+            case 0: 
+                addForce.x = 0f; break;
+            case 1:
+                addForce.x = -1f; break;
+            case 2:
+                addForce.x = +1f; break;
+        }
 
-        float moveSpeed = 2f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        int moveZ = actions.DiscreteActions[1];
+        switch (moveZ)
+        {
+            case 0:
+                addForce.z = 0f; break;
+            case 1:
+                addForce.z = -1f; break;
+            case 2:
+                addForce.z = +1f; break;
+        }
+
+        float movementSpeed = 2f;
+        agentRigidBody.velocity = addForce * movementSpeed + new Vector3(0, agentRigidBody.velocity.y, 0);
+
+        bool isButtonPressed = actions.DiscreteActions[2] == 1;
+        if (isButtonPressed)
+        {
+            Collider[] colliderArr = Physics.OverlapBox(transform.position, Vector3.one * 0.5f);
+            foreach (Collider collider in colliderArr)
+            {
+                if (collider.TryGetComponent<Button>(out _))
+                {
+                    if (isButtonAvailable)
+                    {
+                        button.SetActive(false);
+                        isButtonAvailable = false;
+                        barrier.SetActive(false);
+                        isBarrierGone = true;
+                        AddReward(+1f);
+                    }
+                }
+            }
+        }
+
+        AddReward(-1f / MaxStep);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[1] = Input.GetAxisRaw("Vertical");
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+
+        // Move left & right
+        int horizontalInput = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        switch (horizontalInput)
+        {
+            case -1:
+                discreteActions[0] = 1; break;
+            case 0:
+                discreteActions[0] = 0; break;
+            case +1:
+                discreteActions[0] = 2; break;
+        }
+
+        // Move up & down
+        int verticalInput = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+        switch (verticalInput)
+        {
+            case -1:
+                discreteActions[1] = 1; break;
+            case 0:
+                discreteActions[1] = 0; break;
+            case +1:
+                discreteActions[1] = 2; break;
+        }
+
+        // Press button
+        discreteActions[2] = Input.GetKey(KeyCode.E) ? 1 : 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<Button>(out Button target))
+        if (other.TryGetComponent<Target>(out Target target))
         {
             agentMeshRenderer.material = rewardMaterial;
-            SetReward(+1f);
+            AddReward(+1f);
             EndEpisode();
         }
+
+        // TODO: delete code before submission
         if (other.TryGetComponent<Wall>(out Wall wall))
         {
             agentMeshRenderer.material = penaltyMaterial;
-            SetReward(-1f);
+            AddReward(-1f);
             EndEpisode();
         }
     }
