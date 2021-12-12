e1558db2dbd108c18bee632536553f842e515fc0
qianwang223 <leviwangqian@gmail.com>
2021-12-12 13:44:59 -0800

    Some movement changes

--- Project/Assets/ML-Agents/Scripts/Button.cs
+++ Project/Assets/ML-Agents/Scripts/Button.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,54 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    /*
+    public event EventHandler Use;
+    [SerializeField] private Material green;
+    [SerializeField] private Material darkGreen;
+
+    private MeshRenderer buttonMeshRender;
+    private Transform buttonTransform;
+    private bool canUseButton;
+
+    private void Awake()
     {
-        
+
+      
+        buttonTransform = transform.Find("a");
+        buttonMeshRender = buttonTransform.GetComponent<MeshRenderer>();
+        canUseButton = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ResetButton();
     }
+
+
+    public bool CanUseButton() {
+        return canUseButton;
+    }
+
+
+    public void UseButton() {
+        if (canUseButton) {
+            buttonMeshRender.material = darkGreen;
+            buttonTransform.localScale = new Vector3(.5f, .2f, .5f);
+            canUseButton = false;
+
+            Use?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void ResetButton() {
+        buttonMeshRender.material = green;
+        buttonTransform.localScale = new Vector3(.5f, .5f, .5f);
+
+        transform.localPosition = new Vector3(UnityEngine.Random.Range(-2f, +2f), 0, UnityEngine.Random.Range(-0.6f, +0.6f));
+        canUseButton = true;
+    }
+    */
+    // Update is called once per frame
+
 }
--- Project/Assets/ML-Agents/Scripts/CubeAgent.cs
+++ Project/Assets/ML-Agents/Scripts/CubeAgent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,36 +8,103 @@ using Unity.MLAgents.Sensors;
 
 public class CubeAgent : Agent
 {
+    // public event EventHandler eat;
+     public event EventHandler beginEvent;
     [SerializeField] private Transform targetTransform;
     [SerializeField] private Material rewardMaterial;
     [SerializeField] private Material penaltyMaterial;
     [SerializeField] private MeshRenderer agentMeshRenderer;
 
+   // [SerializeField] private Button button;
+
+    private Rigidbody agentRigidbody;
+
+    
+    private void Awake()
+    {
+        agentRigidbody = GetComponent<Rigidbody>();
+    }
+    
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = Vector3.zero;
+        transform.localPosition = new Vector3(UnityEngine.Random.Range(-2f, +0.6f), 0, UnityEngine.Random.Range(-0.3f, +1.5f));
+         targetTransform.localPosition = new Vector3(UnityEngine.Random.Range(-2f, +2f), 0, UnityEngine.Random.Range(-0.6f, +0.6f));
+        beginEvent?.Invoke(this, EventArgs.Empty);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
+        /*
+        sensor.AddObservation(button.CanUseButton() ? 1 : 0);
+
+        Vector3 dirToButton = (button.transform.localPosition - transform.localPosition).normalized;
+        sensor.AddObservation(dirToButton.x);
+        sensor.AddObservation(dirToButton.z);
+        */
         sensor.AddObservation(transform.position);
         sensor.AddObservation(targetTransform.position);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
+         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
+         float moveSpeed = 2f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+
+        /*
+        float moveX = actions.DiscreteActions[0];
+        float moveZ = actions.DiscreteActions[1];
+
+        Vector3 addForce = new Vector3(0, 0, 0);
+
+        switch (moveX) {
+            case 0: addForce.x = 0f; break;
+            case 1: addForce.x = -1f; break;
+            case 2: addForce.x = +1f; break;
+        }
+
+        switch (moveZ)
+        {
+            case 0: addForce.z = 0f; break;
+            case 1: addForce.z = -1f; break;
+            case 2: addForce.z = +1f; break;
+        }
 
         float moveSpeed = 2f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        agentRigidbody.velocity = addForce * moveSpeed + new Vector3(0, agentRigidbody.velocity.y, 0);
+        */
+
+
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[1] = Input.GetAxisRaw("Vertical");
+         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+         continuousActions[0] = Input.GetAxisRaw("Horizontal");
+          continuousActions[1] = Input.GetAxisRaw("Vertical");
+
+
+        /*
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        switch (Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")))
+        {
+            case -1: discreteActions[0] = 1; break;
+            case 0: discreteActions[0] = 0; break;
+            case +1: discreteActions[0] = 2; break;
+        }
+
+        switch (Mathf.RoundToInt(Input.GetAxisRaw("Vertical")))
+        {
+            case -1: discreteActions[1] = 1; break;
+            case 0: discreteActions[1] = 0; break;
+            case +1: discreteActions[1] = 2; break;
+        }
+
+        discreteActions[2] = Input.GetKey(KeyCode.E) ? 1 : 0;
+        */
+
     }
 
     private void OnTriggerEnter(Collider other)
