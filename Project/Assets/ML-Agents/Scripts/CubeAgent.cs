64da03083a67a37b1661cc719bb30c25017bdafc
Ayman Faisal <35163891+ayman-faisal@users.noreply.github.com>
2021-12-12 03:13:13 -0800

    Implemented demo agent
    Can move the agent around using mouse & keyboard.
    Had to make the bridge wider as the agent is a bit too big.

--- Project/Assets/ML-Agents/Scripts/CubeAgent.cs
+++ Project/Assets/ML-Agents/Scripts/CubeAgent.cs
@@ -11,7 +11,7 @@ public class CubeAgent : Agent
 
     public override void OnEpisodeBegin()
     {
-        transform.position = Vector3.zero;
+        transform.localPosition = Vector3.zero;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -25,13 +25,30 @@ public class CubeAgent : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-        float moveSpeed = 1f;
+        float moveSpeed = 2f;
         transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        SetReward(1f);
-        EndEpisode();
+        if (other.TryGetComponent<Button>(out Button target))
+        {
+            Debug.Log("Collided with button");
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            Debug.Log("Collided with wall");
+            SetReward(-1f);
+            EndEpisode();
+        }
     }
 }
