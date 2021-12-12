897febba44a75037188ed7aecb5b06a9bc4de641
Ayman Faisal <35163891+ayman-faisal@users.noreply.github.com>
2021-12-12 01:18:22 -0800

    Updated agent environment & Added basic code

---
+++ Project/Assets/ML-Agents/Scripts/CubeAgent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class CubeAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 1f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetReward(1f);
+        EndEpisode();
+    }
+}
